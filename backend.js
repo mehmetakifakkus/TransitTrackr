dbe6160b1721aed336411613d1980147243c3a75
Utku Kaynar <utku@buck.ai>
2023-11-09 14:01:27 -0600

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,15 +1,12 @@
-// For the candidate, this is a sample backend written in Node.js for a real-time backend that emits the location of vehicles on a route over WebSockets. Feel free to change the sendLocationUpdates to your liking. 
 // Import necessary modules
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 const mongoose = require('mongoose');
-const passport = require('passport');
 const cors = require('cors');
 
-// Import your route and user schemas
+// Import your route schema
 // const Route = require('./models/Route');
-// const User = require('./models/User');
 
 // Initialize the express application
 const app = express();
@@ -31,14 +28,8 @@ mongoose.connect('your-mongodb-uri', { useNewUrlParser: true, useUnifiedTopology
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-// Passport middleware for authentication
-app.use(passport.initialize());
-require('./config/passport')(passport); // Import and configure passport strategies
-
 // Define the RESTful API routes
 app.get('/api/routes', (req, res) => {
-  // Route.find() will fetch all the route data from your MongoDB
-  // Assuming you have a Route model already set up
   Route.find()
     .then(routes => res.json(routes))
     .catch(err => res.status(404).json({ noroutesfound: 'No routes found' }));
@@ -50,24 +41,32 @@ app.get('/api/routes/:id', (req, res) => {
     .catch(err => res.status(404).json({ noroutefound: 'No route found with that ID' }));
 });
 
-// Authentication Routes would go here
-// ...
+// Function to generate random coordinates in San Jose, California
+const generateRandomCoordinates = () => {
+  // San Jose approximate bounds
+  const latMin = 37.1245;
+  const latMax = 37.5485;
+  const lngMin = -122.2654;
+  const lngMax = -121.5891;
+
+  const lat = Math.random() * (latMax - latMin) + latMin;
+  const lng = Math.random() * (lngMax - lngMin) + lngMin;
+  
+  return { lat, lng };
+};
 
 // WebSocket connection logic
 io.on('connection', (socket) => {
   console.log('New client connected');
 
-  // Emit real-time updates for a specific route
-  // You would replace this with your actual real-time update logic
   const sendLocationUpdate = () => {
-    const fakeLocationData = {
-      routeId: '12345',
-      coordinates: { lat: 40.7128, lng: -74.0060 } // Replace with real data
+    const locationData = {
+      routeId: '12345',  // Example route ID
+      coordinates: generateRandomCoordinates() // Generates random coordinates in San Jose
     };
-    socket.emit('locationUpdate', fakeLocationData);
+    socket.emit('locationUpdate', locationData);
   };
 
-  // Call sendLocationUpdate at your chosen interval
   const locationUpdateInterval = setInterval(sendLocationUpdate, 10000);
 
   socket.on('disconnect', () => {
