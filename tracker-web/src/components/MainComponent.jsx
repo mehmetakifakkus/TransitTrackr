4d21c310fe3f9aba4a451eaa096699c0b359d9f3
Mehmet Akif AKKUŞ <mehmetakifakkus@gmail.com>
2023-11-13 22:28:32 +0300

    Add MainComponent includes routelist and map

---
+++ tracker-web/src/components/MainComponent.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+import { Map } from './Map'
+import '../styles/MainComponent.scss'
+import RouteList from './RouteList';
+import axios from 'axios';
+
+function MainComponent({socket}) {
+  const [routes, setRoutes] = useState([]);
+  const [locationUpdates, setLocationUpdates] = useState([]);
+
+  useEffect(()=>{
+    axios.get("http://localhost:5001/api/routes")
+    .then(response => {
+      setRoutes(response.data);
+      setLocationUpdates(response.data);
+    }).catch(err => {
+      console.error('Error while fetching routes!');
+    })
+  }, []);
+  
+  useEffect(() => {
+    socket?.on("locationUpdate", (updates) => {
+      const merged = updates.map(update => {
+        const route = routes?.find(route => route.id.includes(update.id));
+        return {
+          ...update, 
+          origin: route?.origin,
+        }
+      })
+      setLocationUpdates(merged);
+      console.log(merged);
+    })
+  }, [socket, routes]);
+
+  return (
+    <div className='main'>
+      <RouteList socket={socket} routes={routes} locationUpdates={locationUpdates}  />
+      <Map routes={routes} locationUpdates={locationUpdates}/>
+    </div>
+  )
+}
+
+export default MainComponent
