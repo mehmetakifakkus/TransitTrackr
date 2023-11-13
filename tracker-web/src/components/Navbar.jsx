861f3c4a7f1f6403f53a4b2815df40671e45d0ff
Mehmet Akif AKKUŞ <mehmetakifakkus@gmail.com>
2023-11-13 22:28:58 +0300

    Add website navbar and styles

---
+++ tracker-web/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles/Navbar.scss'
+
+function Navbar() {
+  return (
+    <nav className='navbar'>
+      <span className='logo'>Simultaneous Route Tracking</span></nav>
+  )
+}
+
+export default Navbar
